08b440f87c04e85fbfdeb26170765b626742549d
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-09-22 14:07:31 +0630

    change package.json file

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bodyParser: false,
-  });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true
-  }));
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
 
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    }));
+
+    const port = process.env.PORT ?? 3000;
+    await app.listen(port);
+
+    logger.log(`Application is running on: http://localhost:${port}`);
+  } catch (error) {
+    logger.error('Error starting application:', error);
+    process.exit(1);
+  }
 }
 bootstrap();
